7b76dd0495814822a963ff9fb236c94f01594111
GlamFazbear1987 <146845967+GlamFazbear1987@users.noreply.github.com>
2024-10-09 12:40:02 -0400

    Create with Code 2.3

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[ ] animalPrefabs;
+    public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
    
@@ -20,9 +20,9 @@ public class SpawnManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.S))
         {
             //Randomly generate aniamlm index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawn.RangeX, spawnRangeX), 0, spawnPosZ);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos,animalPrefabs[animalIndex].transition.rotation);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
